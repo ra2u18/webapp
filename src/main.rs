9cccd43f6c15399c24352f4a30be89f6f9e96459
ra2u181 <netlemon2021@gmail.com>
2022-08-12 11:52:45 +0300

    Added docker support

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use zero2prod::{
     telemetry::{get_subscriber, init_subscriber},
 };
 
-use sqlx::PgPool;
+use sqlx::{PgPool, postgres::PgPoolOptions};
 use std::net::TcpListener;
 
 #[tokio::main]
@@ -15,13 +15,12 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
-    // Panic if we can't read the config
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres");
 
-    // We have removed the hard-coded 8000 - it's now coming from our settings
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
