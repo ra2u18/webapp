95fd237a1a052de1a6c3c7a133b0890878ac7c1e
ra2u181 <netlemon2021@gmail.com>
2022-08-24 16:42:44 +0300

    implemented thiserror

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -124,7 +124,7 @@ async fn subscribe_fails_if_there_is_a_fatal_database_error() {
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
     // Sabotage the db
-    sqlx::query!("ALTER TABLE subscriptions_tokens DROP COLUMN subscription_token;")
+    sqlx::query!("ALTER TABLE subscriptions DROP COLUMN email;")
         .execute(&app.db_pool)
         .await
         .unwrap();
