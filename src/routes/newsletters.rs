93c8c375e2d641bd8bbd73c30fa454bf70bc8eac
ra2u181 <netlemon2021@gmail.com>
2022-08-25 11:57:01 +0300

    added basic authentication with random accounts

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,11 +1,19 @@
 //! src/routes/newsletters.rs
-use crate::{routes::error_chain_fmt, email_client::EmailClient, domain::SubscriberEmail};
-use actix_web::{http::StatusCode, web, HttpResponse, ResponseError};
-use sqlx::PgPool;
+use crate::{domain::SubscriberEmail, email_client::EmailClient, routes::error_chain_fmt};
+use actix_web::{
+    http::header::{self, HeaderMap, HeaderValue},
+    http::StatusCode,
+    web, HttpRequest, HttpResponse, ResponseError,
+};
 use anyhow::Context;
+use secrecy::Secret;
+use sqlx::PgPool;
 
 #[derive(thiserror::Error)]
 pub enum PublishError {
+    // New Error variant
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
     #[error(transparent)]
     UnexpectedError(#[from] anyhow::Error),
 }
@@ -17,9 +25,18 @@ impl std::fmt::Debug for PublishError {
 }
 
 impl ResponseError for PublishError {
-    fn status_code(&self) -> StatusCode {
+    fn error_response(&self) -> HttpResponse {
         match self {
-            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR
+            PublishError::UnexpectedError(_) => HttpResponse::new(StatusCode::UNAUTHORIZED),
+            PublishError::AuthError(_) => {
+                let mut response = HttpResponse::new(StatusCode::UNAUTHORIZED);
+                let header_value = HeaderValue::from_str(r#"Basic realm="publish""#).unwrap();
+                response
+                    .headers_mut()
+                    .insert(header::WWW_AUTHENTICATE, header_value);
+
+                response
+            }
         }
     }
 }
@@ -43,8 +60,12 @@ struct ConfirmedSubscriber {
 pub async fn publish_newsletter(
     body: web::Json<BodyData>,
     pool: web::Data<PgPool>,
-    email_client: web::Data<EmailClient>
+    email_client: web::Data<EmailClient>,
+    // New extractor
+    request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
+    let _credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
+
     let subscribers = get_confirmed_subscribers(&pool).await?;
 
     for subscriber in subscribers {
@@ -56,13 +77,13 @@ pub async fn publish_newsletter(
                         &subscriber.email,
                         &body.title,
                         &body.content.html,
-                        &body.content.text
+                        &body.content.text,
                     )
                     .await
                     .with_context(|| {
                         format!("Failed to send newsletter issue to {}", subscriber.email)
                     })?;
-            },
+            }
             Err(error) => {
                 tracing::warn!(
                     error.cause_chain = ?error,
@@ -71,7 +92,6 @@ pub async fn publish_newsletter(
                 );
             }
         }
-
     }
 
     Ok(HttpResponse::Ok().finish())
@@ -98,4 +118,43 @@ async fn get_confirmed_subscribers(
     .collect();
 
     Ok(confirmed_subscribers)
+}
+
+struct Credentials {
+    username: String,
+    password: Secret<String>,
+}
+
+fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
+    // The header value, if present must be a valid UTF-8 string
+    let header_value = headers
+        .get("Authorization")
+        .context("The 'Authorization' header was missing")?
+        .to_str()
+        .context("The 'Authorization' header was not a valid UTF-8 string")?;
+
+    let base64encoded_seg = header_value
+        .strip_prefix("Basic ")
+        .context("The 'Authorization' scheme was not 'Basic'")?;
+
+    let decoded_bytes = base64::decode_config(base64encoded_seg, base64::STANDARD)
+        .context("Failed to base64-decode 'Basic' credentials")?;
+    let decoded_credentials = String::from_utf8(decoded_bytes)
+        .context("The decoded credential string is not valid UTF8")?;
+
+    // Split into 2 segments
+    let mut credentials = decoded_credentials.splitn(2, ':');
+    let username = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A username must be provided in 'Basic' auth"))?
+        .to_string();
+    let password = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A password must be provided in 'Basic' auth"))?
+        .to_string();
+
+    Ok(Credentials {
+        username,
+        password: Secret::new(password),
+    })
 }
